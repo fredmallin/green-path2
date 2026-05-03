6ca38bb23a5cc3c6d46032dbd7d873490c8a2990
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-03 12:24:28 +0300

    initialize my project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function AppContent() {
   const { isAuthenticated, loading } = useAuth();
   const [view, setView] = useState("hero");
 
-  // 🔥 Wait for Firebase 
+  // 🔥 Wait for Firebas
   if (loading) {
     return <div>Loading...</div>;
   }
