40bf4541abea16fa33e99648edef4b4f9d39ce94
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-03 12:26:43 +0300

    initialize my project

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -52,7 +52,7 @@ export function Register({ onSuccess, onSwitchToLogin }) {
     }
 
     try {
-      // 🔥 1. CREATE AUTH USER
+      // 🔥 1. CREATE AUTH USE
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         formData.email,
