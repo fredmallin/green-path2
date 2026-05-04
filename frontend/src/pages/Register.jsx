461a7e2d874aac10f3dfa53279a242620edb051c
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-04 22:44:51 +0300

    initialize my project

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -61,7 +61,7 @@ export function Register({ onSuccess, onSwitchToLogin }) {
 
       const user = userCredential.user;
 
-      //  2. STORE EXTRA DATA IN FIRESTOR
+      //  2. STORE EXTRA DATA IN FIRESTO
       await setDoc(doc(db, "users", user.uid), {
         uid: user.uid,
         name: formData.name,
