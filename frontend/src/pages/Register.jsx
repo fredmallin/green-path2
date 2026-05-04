afb93e07240a5e85af1d1f5758fc083e3b7f41d6
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-04 22:46:16 +0300

    initialize my project

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -61,7 +61,7 @@ export function Register({ onSuccess, onSwitchToLogin }) {
 
       const user = userCredential.user;
 
-      //  2. STORE EXTRA D
+      //  2. STORE EXTRA
       await setDoc(doc(db, "users", user.uid), {
         uid: user.uid,
         name: formData.name,
