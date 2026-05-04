086839092d4bdd03f120843981b18e64693e3a15
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-04 22:53:27 +0300

    initialize my project

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -76,7 +76,7 @@ export function Register({ onSuccess, onSwitchToLogin }) {
       setSuccess(true);
 
       setTimeout(() => {
-        onSuccess(); // redirect to login/d
+        onSuccess(); // redirect to login
       }, 2000);
 
     } catch (err) {
