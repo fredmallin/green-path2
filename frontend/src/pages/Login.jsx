948f0e255319c57ab7edf4224b0154eabad17ee0
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-05 21:50:32 +0300

    initialize my project

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Leaf, Mail, Lock } from "lucide-react";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase"; // ad
+import { auth } from "../firebase"; // a
 
 export function Login({ onSuccess, onSwitchToRegister }) {
   const [formData, setFormData] = useState({
