f55a7d66a1d20b2aa13a221c49346bb4f9dc3c2b
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-14 19:47:16 +0300

    initialize my project

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -42,7 +42,7 @@ export function Login({ onSuccess, onSwitchToRegister }) {
       console.log("Logged in user:", userCredential.user.email);
 
       setLoading(false);
-      onSuccess(); // move to ap
+      onSuccess(); // move to a
 
     } catch (err) {
       setLoading(false);
