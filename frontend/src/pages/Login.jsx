6303e22a2e70f0fc0c8d1b2468ffcc5261be3ac7
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-14 19:46:45 +0300

    initialize my project

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -42,7 +42,7 @@ export function Login({ onSuccess, onSwitchToRegister }) {
       console.log("Logged in user:", userCredential.user.email);
 
       setLoading(false);
-      onSuccess(); // move to app/dashb
+      onSuccess(); // move to app/das
 
     } catch (err) {
       setLoading(false);
